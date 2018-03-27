c4b9d8b55dcf771082d10ec94fda726fee1f4676
Jeff <jeffrey.karle@gmail.com>
2018-03-26 19:22:24 -0500

    gravity still doesn't work, only had one light

--- cinder/gravity.h
+++ cinder/gravity.h
@@ -15,29 +15,30 @@ uint8_t atBottom = 0;
 uint8_t timer = 0;
 //uint8_t pause = 100;
 uint8_t palette_index;
+uint8_t count = 0;
 
 void gravity() {                                                              // This is the heart of this_ program. Sure is short.
 
-  if (timer == 0) {x = x0;}
+  if (timer == 0) {x_grav = x0;}
   
   x_prev = x_grav;
   x_grav = x0 + v0 * timer + 0.5 * accel * timer * timer;
   timer += this_delay / 1000;
   
-  if (x <= 0) {		// Handle x going below 0
+  if (x_grav <= 0) {		// Handle x going below 0
 	  count +=1;	// Create a delay before resetting
-	  x = 0;
+	  x_grav = 0;
   }
   if (count == pause) { // Once we reach the time for pause, reset
 	  timer = 0;
+    count = 0;
   }
-  palette_index = x * 255 / 143;
+  palette_index = x_grav * 255 / 143;
   
   ringCRGB(x_prev,0,0,0); // maybe use fadeToBlackBy?
   //ringCRGB(x,255,0,0);
-  ringPalette(x, current_palette, palette_index, this_bright, current_blending);
+  ringPalette(x_grav, current_palette, palette_index, this_bright, current_blending);
 
-  }
 } // gravity()
 
 #endif
